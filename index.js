9e251857ab8ebc5813fab2c3e3c043087adebea4
auliaaptrs <auliaps135@gmail.com>
2024-04-03 13:30:18 +0700

    update index.js

--- index.js
+++ index.js
@@ -8,16 +8,25 @@ app.use(express.json());
 app.get("/", (req, res) => {
     const htmlResponse = `
         <html>
-            <head>
-                <title>Homepage</title>
-            </head>
-            <body>
-                <center>
-                    <h1>HALO NCC!</h1>
-                    <a href="/about">LETSGO!</a>
-                </center>
-            </body>
-        </html>
+        <head>
+            <title>Homepage</title>
+            <link rel="stylesheet" type="text/css" href="styles.css">
+        </head>
+        <body>
+        <center>
+            <header>
+                <h1>TOKO SEMBAKO SINAR JAYA</h1>
+                <p>Senin - Jumat: 08:00 - 23:59</p>
+                <p>Sabtu: 08:00 - 20:59</p>
+                <p>Minggu: 08:00 - 17:59</p>
+            </header>
+            <nav>
+                    <a href="/about">ABOUT</a>
+                    <a href="/db-mysql">DATA PRODUK</a>
+            </nav>
+        <center>
+        </body>
+    </html>
     `;
     res.send(htmlResponse);
 });
@@ -29,12 +38,15 @@ app.get("/about", (req, res) => {
                 <title>ABOUT</title>
             </head>
             <body>
+            <center>
                 <h1>ABOUT</h1>
-                <h3>Perkenalkan..</h3>
-                <h3>Saya Aulia Putri Salsabila</h3>
-                <h3>Asal Jakarta</h3>
-                <h3>Angkatan 2022</h3>
-                
+                <h3>TOKO SEMBAKO SINAR JAYA</h3>
+                <p>Toko Sembako Sinar Jaya menyediakan berbagai macam kebutuhan pokok dan keperluan rumah tangga sejak tahun 1945.</p>
+                <p>Toko ini didirikan oleh Ibu Aulia dan saat ini telah berkembang menjadi toko sembako terkemuka di wilayah sekitar.</p>
+                <p>Kami menjual berbagai jenis produk sembako seperti beras, gula, minyak goreng, tepung, serta barang-barang kebutuhan rumah tangga lainnya.</p>
+                <p>Saat ini, Toko Sembako Sinar Jaya memiliki 5 cabang yang tersebar di berbagai lokasi strategis di ibu kota.</p>
+                <p>Dapatkan lebih banyak kemudahan melalui website resmi kami untuk mengecek ketersediaan produk yang berkualitas!</p>
+            <center>
             </body>
         </html>
     `;
@@ -42,9 +54,10 @@ app.get("/about", (req, res) => {
 });
 
 
+
 // GET data dari MySQL
 app.get("/db-mysql", (req, res) => {
-    const querySql = 'SELECT * FROM biodata';
+    const querySql = 'SELECT *, IF(tersedia = 1, "True", "False") AS tersedia FROM produk';
     dbconnection.query(querySql, (err, rows) => {
         if (err) {
             return res.status(500).json({ message: 'There is an error', error: err });
@@ -56,13 +69,20 @@ app.get("/db-mysql", (req, res) => {
 // GET data by id dari MySQL
 app.get("/db-mysql/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    const querySql = `SELECT * FROM biodata WHERE id = ${id}`;
+    const querySql = `SELECT *, IF(tersedia = 1, "True", "False") AS tersedia FROM produk WHERE id = ${id}`;
     dbconnection.query(querySql, (err, rows) => {
         if (err) {
             return res.status(500).json({ message: 'There is an error', error: err });
         }
         if (rows.length === 0) {
-            return res.status(404).json({ message: 'Data not found' });
+            return res.status(404).json({
+                "errors": {
+                  "id": [
+                    "Tidak ditemukan. Pastikan id yang anda masukkan benar!"
+                  ]
+                }
+              }
+              );
         }
         res.status(200).json({ data: rows[0] });
     });
@@ -70,60 +90,110 @@ app.get("/db-mysql/:id", (req, res) => {
 
 // Create new data di MySQL
 app.post("/db-mysql", (req, res) => {
-    const { id, Nama, Asal, Angkatan } = req.body;
-    // Periksa apakah semua field yang dibutuhkan ada dalam body request
-    if (!id || !Nama || !Asal || !Angkatan) {
-        return res.status(400).json({ message: 'ID, Nama, asal, dan angkatan harus diisi' });
+    const { id, nama, harga, tersedia, stok, deskripsi } = req.body;
+
+    const errors = {};
+    if (!id) {
+        errors.id = ['Data belum diisi'];
+    }
+    if (!nama) {
+        errors.nama = ['Data belum diisi'];
+    }
+    if (!harga) {
+        errors.harga = ['Data belum diisi'];
+    }
+    if (!tersedia) {
+        errors.tersedia = ['Data belum diisi'];
+    }
+    if (!stok) {
+        errors.stok = ['Data belum diisi'];
+    }
+    if (!deskripsi) {
+        errors.deskripsi = ['Data belum diisi'];
     }
 
-    // Gunakan placeholder untuk mencegah serangan SQL injection
-    const querySql = 'INSERT INTO biodata (id, Nama, Asal, Angkatan) VALUES (?, ?, ?, ?)';
-    dbconnection.query(querySql, [id, Nama, Asal, Angkatan], (err, result) => {
+    if (Object.keys(errors).length > 0) {
+        return res.status(400).json({ errors });
+    }
+
+
+    const querySql = 'INSERT INTO produk (id, nama, harga, tersedia, stok, deskripsi) VALUES (?, ?, ?, ?, ?, ?)';
+    dbconnection.query(querySql, [id, nama, harga, tersedia, stok, deskripsi], (err, result) => {
         if (err) {
             return res.status(500).json({ message: 'There is an error', error: err });
         }
-        res.status(201).json({ message: 'Data created successfully', data: { id, Nama, Asal, Angkatan } });
+        res.status(201).json({ message: 'Data created successfully', data: { id, nama, harga, tersedia: tersedia ? "True" : "False", stok, deskripsi } });
     });
 });
-
-
-
-
 // Update data by id di MySQL
 app.put("/db-mysql/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    const { Nama, Asal, Angkatan } = req.body;
+    const { nama, harga, tersedia, stok, deskripsi } = req.body;
+    
     // Validasi angka id
     if (isNaN(id)) {
-        return res.status(400).json({ message: 'ID harus berupa angka' });
+        return res.status(400).json({ message: 'id harus berupa angka' });
     }
-    // Periksa apakah Nama, Asal, dan Angkatan ada dalam body request
-    if (!Nama || !Asal || !Angkatan) {
-        return res.status(400).json({ message: 'Nama, Asal, dan Angkatan harus diisi' });
+    
+    // Validasi apakah setiap field sudah diisi
+    const errors = {};
+    if (!nama) {
+        errors.nama = ["Data belum diisi"];
     }
+    if (!harga) {
+        errors.harga = ["Data belum diisi"];
+    }
+    if (tersedia === undefined) {
+        errors.tersedia = ["Data belum diisi"];
+    }
+    if (!stok) {
+        errors.stok = ["Data belum diisi"];
+    }
+    if (!deskripsi) {
+        errors.deskripsi = ["Data belum diisi"];
+    }
+    
+    // Jika terdapat error, kembalikan respons dengan format yang sesuai
+    if (Object.keys(errors).length > 0) {
+        return res.status(400).json({ errors });
+    }
+
     // Gunakan placeholder untuk mencegah serangan SQL injection
-    const querySql = 'UPDATE biodata SET Nama=?, Asal=?, Angkatan=? WHERE id=?';
-    dbconnection.query(querySql, [Nama, Asal, Angkatan, id], (err, result) => {
+    const querySql = 'UPDATE produk SET nama=?, harga=?, tersedia=?, stok=?, deskripsi=? WHERE id=?';
+    dbconnection.query(querySql, [nama, harga, tersedia, stok, deskripsi, id], (err, result) => {
         if (err) {
             return res.status(500).json({ message: 'There is an error', error: err });
         }
         if (result.affectedRows === 0) {
-            return res.status(404).json({ message: 'Data not found' });
+            return res.status(404).json({
+                "errors": {
+                  "id": [
+                    "Tidak ditemukan. Pastikan id yang anda masukkan benar!"
+                  ]
+                }
+              });
         }
-        res.status(200).json({ message: 'Data updated successfully', data: { id, Nama, Asal, Angkatan } });
+        res.status(200).json({ message: 'Data updated successfully', data: { id, nama, harga, tersedia: tersedia ? "True" : "False", stok, deskripsi } });
     });
 });
+
 
 // Delete data by id di MySQL
 app.delete("/db-mysql/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    const querySql = `DELETE FROM biodata WHERE id=${id}`;
+    const querySql = `DELETE FROM produk WHERE id=${id}`;
     dbconnection.query(querySql, (err, result) => {
         if (err) {
             return res.status(500).json({ message: 'There is an error', error: err });
         }
         if (result.affectedRows === 0) {
-            return res.status(404).json({ message: 'Data not found' });
+            return res.status(404).json({
+                "errors": {
+                  "id": [
+                    "Tidak ditemukan. Pastikan id yang anda masukkan benar!"
+                  ]
+                }
+              });
         }
         res.status(200).json({ message: 'Data deleted successfully' });
     });
